ab59c14260e370c99c1f4f05c91c4b74ac864453
depechedave <depechedave@gmail.com>
2022-03-31 14:26:25 +0100

    added route useParams

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function BlogList({ blogs }) {
   return (
     <div className="blog-list">
       <h2>Demo React Blog</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+          </Link>
         </div>
       ))}
     </div>
