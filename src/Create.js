31b7497a58153d8db1ed4e76a9c0786e1a7990e1
depechedave <depechedave@gmail.com>
2022-03-31 16:37:09 +0100

    added useNavigate to Homepage on addition

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Create() {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("Dave");
   const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,6 +19,7 @@ export default function Create() {
       body: JSON.stringify(blog),
     }).then(() => {
       setIsPending(false);
+      navigate("/");
     });
   };
 
