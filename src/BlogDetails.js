f395043fcc498f380c198cf6b55ef795966d7dc1
depechedave <depechedave@gmail.com>
2022-03-31 20:16:33 +0100

    added delete to blogs

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,13 +1,22 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "./useFetch";
 
 export default function BlogDetails() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const {
     data: blog,
     error,
     isPending,
   } = useFetch("http://localhost:8000/blogs/" + id);
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + id, {
+      method: "DELETE",
+    }).then(() => {
+      navigate("/");
+    });
+  };
+
   return (
     <div className="blog-details">
       {isPending && <div>Loading...</div>}
@@ -17,6 +26,7 @@ export default function BlogDetails() {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
