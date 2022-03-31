fc4f34c269dec5de29e649a153e8f7b0ddc48018
depechedave <depechedave@gmail.com>
2022-03-31 15:26:58 +0100

    added new blog form

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 export default function Create() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("mario");
+
   return (
     <div className="create">
-      <h2>Add a new Blog</h2>
+      <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog author:</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="dave">Dave</option>
+          <option value="john">John</option>
+        </select>
+        <button>Add Blog</button>
+      </form>
     </div>
   );
 }
